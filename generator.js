9069528ff7968931271118c4b1d998cd7cb9dd33
jonschlinkert <jon.schlinkert@sellside.com>
2016-07-27 05:49:09 -0400

    update paths

--- generator.js
+++ generator.js
@@ -1,15 +1,14 @@
 'use strict';
 
 var path = require('path');
-var dir = path.resolve.bind(path, __dirname, 'templates');
+var templates = path.join(__dirname, 'templates');
 var isValid = require('is-valid-app');
 
 module.exports = function(app) {
   if (!isValid(app, 'generate-eslint')) return;
 
   /**
-   * Generate a `.eslintrc.json` file to the current working directory or
-   * specified `--dest` directory.
+   * Generate a `.eslintrc.json` file to the current working directory.
    *
    * ```sh
    * $ gen eslint
@@ -18,19 +17,16 @@ module.exports = function(app) {
    * @api public
    */
 
-  app.task('eslint', function() {
-    return app.src(dir('eslintrc.json'))
+  app.task('eslint', {silent: true}, function() {
+    return app.src('_eslintrc.json', {cwd: templates})
       .pipe(app.dest(function(file) {
         file.basename = '.eslintrc.json';
-        return app.options.dest || app.cwd;
+        return app.cwd;
       }));
   });
 
   /**
-   * Generate a `.eslintignore` file to the current working directory or
-   * specified `--dest` directory. This task is also aliased as `eslintignore`,
-   * in case you want to use this generator as a sub-generator or plugin and want
-   * to use the `ignore` task name for something else.
+   * Generate a `.eslintignore` file to the current working directory. This task is also aliased as `eslintignore`, in case you want to use this generator as a sub-generator or plugin and want to use the `ignore` task name for something else.
    *
    * ```sh
    * $ gen eslint:ignore
@@ -39,12 +35,12 @@ module.exports = function(app) {
    * @api public
    */
 
-  app.task('ignore', ['eslintignore']);
-  app.task('eslintignore', function() {
-    return app.src(dir('eslintignore'))
+  app.task('ignore', {silent: true}, ['eslintignore']);
+  app.task('eslintignore', {silent: true}, function() {
+    return app.src('_eslintignore', {cwd: templates})
       .pipe(app.dest(function(file) {
         file.basename = '.eslintignore';
-        return app.options.dest || app.cwd;
+        return app.cwd;
       }));
   });
 
